aa8dbdbd63147f0efbb5f0e8c4208e8ca4cbe379
sl-81 <s_li1992@hotmail.com>
2023-11-14 16:34:32 -0800

    set up background

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className="container-fluid absolute bg-white">
+      <img className="w-full h-full opacity-75" src="src/projectimg/DSC03279.jpg" />
+
+      {/* Overlay Divs */}
+      <div className="absolute w-full h-1/6 left-0 top-896 bg-white"/>
+      <div className="w-full h-0 absolute border border-white" style={{ top: '6%' }}></div>
+      <div className="w-1/2 h-0 absolute border border-white" style={{ top: '56%' }}></div>
+
+
+      {/* Navigation Links */}
+      <div className="absolute text-stone-700 xs: text-xs sm:text-sm md:text-lg lg:text-xl font-normal font-['Helvetica']" style={{top: '1.5%', left: '90%'  }}>Projects</div>
+      <div className="absolute text-stone-700 xs: text-xs sm:text-sm md:text-lg lg:text-xl font-normal font-['Helvetica']" style={{top: '1.5%', left: '80%'  }}>Resume</div>
+      <div className="absolute text-stone-700 xs: text-xs sm:text-sm md:text-lg lg:text-xl font-normal font-['Helvetica']" style={{top: '1.5%', left: '70%'  }}>Home</div>
+
+      {/* Introduction */}
+      <div className="intro-container absolute w-[613px] h-[167px] left-[56px] top-[205px]">
+        <span className="text-white text-[50px] font-normal font-['Helvetica']">Shiyu Li<br/></span>
+        <span className="text-white text-4xl font-normal font-['Helvetica']">Student, Software Developer, and Pharmacist<br/></span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* About Section */}
+      <div className="w-[728px] h-[329px] left-[56px] top-[386px] absolute">
+        <span className="text-white text-4xl font-normal font-['Helvetica']">About<br/></span>
+        <span className="text-white text-xl font-normal font-['Helvetica']">Hi, my name is Shiyu, a computer science student enrolled in the Bachelor of Computer Science (Second Degree) program at University of British Columbia.<br/><br/>I previously practiced as a pharmacist for 6 years before making a career change to computer science. I enjoy using my programming skills to solve problems I come across. <br/><br/>I am always looking to explore opportunities in the tech industry and taking on new challenges, so feel free to reach out!</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Social Media Links */}
+      <div style={{width: 340, height: 50, left: 41, top: 935, position: 'absolute', justifyContent: 'center', alignItems: 'flex-start', gap: 45.54, display: 'inline-flex'}}>
+        <img style={{width: 52.46, height: 50}} src="src/icons/icons8-linked-in-100.png" />
+        <img style={{width: 50, height: 50}} src="src/icons/icons8-github-96.png" />
+        <img style={{width: 50, height: 50}} src="src/icons/icons8-instagram-100.png" />
+        <img style={{width: 50, height: 50}} src="src/icons/icons8-mail-100.png" />
+      </div>
+      
+
+  </div>
+
+  );
 }
 
 export default App
