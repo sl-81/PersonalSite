b7b55fe09e6e09eb64fe56be661df233335f171f
sl-81 <s_li1992@hotmail.com>
2023-11-27 21:48:19 -0800

    added hover effects to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,34 +2,25 @@ import '../index.css';
 
 function Navbar() {
     return (
-      <div>
-        <div className="
-          right-4
-          top-6
-          absolute 
-          text-black-700 
-          text-xl 
-          font-normal 
-          font-['Helvetica']">Projects
-        </div>
-        <div className="
-          right-36
-          top-6
-          absolute 
-          text-black-700 
-          text-xl 
-          font-normal 
-          font-['Helvetica']">Resume
-        </div>
-        <div className="
-          right-72
-          top-6
-          absolute 
-          text-black-700 
-          text-xl 
-          font-normal 
-          font-['Helvetica']">Home
-        </div>
+      <div className="
+        top-6
+        absolute 
+        text-black-700 
+        text-xl 
+        font-normal 
+        font-['Helvetica']
+        right-4
+        space-x-8
+        ">
+        <button className="
+        hover:bg-white">Projects
+        </button>
+        <button className="
+        hover:bg-white">Resume
+        </button>
+        <button className="
+        hover:bg-white">Home
+        </button>
       </div>
 
     );
