265e68c1e2fbc05b23eb747ae3ae5cfe4a4a6eb8
sl-81 <s_li1992@hotmail.com>
2023-11-28 22:33:08 -0800

    use hashrouter to fix refresh bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ProjectPage from './components/ProjectPage';
 import Home from './components/Home';
 
